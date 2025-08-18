7aee2013b20b8c5254c74bfcf3b7fd3bbf073b49
Adam Soliman <aes10130@nyu.edu>
2025-08-19 00:07:03 +0400

    force change api file to esmodule.

--- api/contact.mjs
+++ api/contact.mjs
@@ -1,18 +1,17 @@
-// api/contact.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+// api/contact.mjs
 import emailjs from 'emailjs-com';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send('Method not allowed');
 
   const { name, email, message } = req.body;
 
   try {
     await emailjs.send(
-      process.env.EMAILJS_SERVICE_ID!,
-      process.env.EMAILJS_TEMPLATE_ID!,
+      process.env.EMAILJS_SERVICE_ID,
+      process.env.EMAILJS_TEMPLATE_ID,
       { name, email, message },
-      process.env.EMAILJS_PUBLIC_KEY!
+      process.env.EMAILJS_PUBLIC_KEY,
     );
 
     res.status(200).json({ success: true });
